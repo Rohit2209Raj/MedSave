0ed856701bee77be87647fb43fb9beab8c56d9ad
Rohit Raj <rohitrajptn2209@gmail.com>
2026-06-19 23:59:20 +0530

    change in both logic files

--- pdf_extractor.py
+++ pdf_extractor.py
@@ -2,9 +2,10 @@ import pdfplumber
 from llm_text import llm_generate
 
 def extract_text(pdf_path):
+    text=""
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
-            text = page.extract_text()
+            text += page.extract_text()
 
     if text and len(text.strip())>20:
         return text
