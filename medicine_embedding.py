58a184a2683b0ec26b527c98850ee5486ccc862d
Rohit Raj <rohitrajptn2209@gmail.com>
2026-07-25 20:30:09 +0530

    Add medicine_embedding.py

---
+++ medicine_embedding.py
@@ -0,0 +1,45 @@
+import pickle
+import os
+from sentence_transformers import SentenceTransformer,util
+from sqlalchemy import create_engine,text
+
+engine=create_engine('postgresql://postgres:FROM EARTH TO SUN@localhost:5432/MedSave_db')
+
+model=SentenceTransformer('all-MiniLM-L6-v2')
+
+EMBEDDINGS_CACHE = "medicine_embedding.pkl"
+
+def load_medicine_embeddings(force_recompute=False):
+
+    if os.path.exists(EMBEDDINGS_CACHE) and not force_recompute:
+        print(f'EMbedding Present: ')
+        with open(EMBEDDINGS_CACHE,'rb') as f:
+            cached_data=pickle.load(f)
+
+        return cached_data['rows'],cached_data['vectors']
+
+    print('Fetching data from Database')
+    with engine.connect() as conn:
+        result=conn.execute(
+            text("SELECT drug_name,mrp FROM medicine")
+        )
+        rows=result.mappings().all()
+
+    if not rows:
+        raise ValueError("medicine table is empty")
+
+    names = [row["drug_name"] for row in rows]
+    vectors = model.encode(names, show_progress_bar=True)
+
+    cached_data={
+        'rows':rows,
+        'vectors':vectors
+    }
+
+    with open(EMBEDDINGS_CACHE,'wb') as f:
+        pickle.dump(cached_data,f)
+
+    return rows,vectors
+
+
+load_medicine_embeddings(force_recompute=True)
