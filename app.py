3d41fc597be07d5a4fd70305cdf8ffb0f8c1d4a7
Rohit Raj <rohitrajptn2209@gmail.com>
2026-07-30 19:41:42 +0530

    Add download_embedding.py

--- app.py
+++ app.py
@@ -1,34 +1,14 @@
-# import streamlit as st
-# import requests
-
-# st.set_page_config(
-#     page_icon='',
-#     layout='centered',
-#     page_title='MedSave'
-# )
-
-# st.title("MedSave")
-# st.write("Upload your Medical Prescription and get Genric Alternative...")
-
-# prescription = st.file_uploader("Upload your prescription here")
-# if st.button("Submit"):
-#     with st.spinner("Analyzing"):
-#         response=requests.post(
-#             url="http://localhost:8000/upload",
-#             files={"prescription":prescription}
-#         )
-#         # st.success(response.status_code)
-#         st.success(response.json())
-
 import streamlit as st
 import requests
+from download_embedding import ensure_embeddings
 
+
+ensure_embeddings()
 st.set_page_config(
     page_icon='💊',
     layout='centered',
     page_title='MedSave'
 )
-
 st.title("💊 MedSave")
 st.write("Upload your prescription and get generic alternatives with price comparison.")
 
---
+++ download_embedding.py
@@ -0,0 +1,18 @@
+# real_download_embeddings.py
+import os, requests
+
+EMBEDDINGS_CACHE = "medicine_embedding.pkl"
+URL = "https://huggingface.co/datasets/YudiCadini/medicine_embedding/blob/main/medicine_embedding.pkl"
+
+def ensure_embeddings():
+    if not os.path.exists(EMBEDDINGS_CACHE):
+        print("Downloading medicine embeddings...")
+        r = requests.get(URL)
+        r.raise_for_status()
+        with open(EMBEDDINGS_CACHE, "wb") as f:
+            f.write(r.content)
+        print("Done.")
+    else:
+        print("Embeddings already present.")
+
+ensure_embeddings()
